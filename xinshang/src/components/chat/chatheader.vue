20a742b0bbeff273c9013f1971c7fbffce7c5653
youqishiyue <1289960360@qq.com>
2018-06-28 21:24:33 +0800

    初次上传

---
+++ xinshang/src/components/chat/chatheader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h1>消息</h1>
+    <i class="iconfont icon-ziyuan"></i>
+  </header>
+</template>
+
+<script>
+    export default {
+        name: "chatheader"
+    }
+</script>
+
+<style scoped>
+header{
+  width: 100%;
+  height: 1.45rem;
+  background: #fff;
+  display: flex;
+  justify-content:space-between ;
+
+}
+header h1{
+  font-size: 28px;
+  color: #474747;
+  height: 1.45rem;
+  line-height: 1.45rem;
+  padding-left: 0.2rem;
+}
+header i{
+  font-size: 20px;
+  color: #474747;
+  padding-right: 0.2rem;
+  padding-top: 0.1rem;
+}
+</style>
---
+++ xinshang/src/pages/chat.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="chat">
+      <chatheader></chatheader>
+      <chatsection></chatsection>
+    </div>
+</template>
+
+<script>
+    import Chatheader from "../components/chat/chatheader";
+    import Chatsection from "../components/chat/chatsection";
+    export default {
+        name: "chat",
+      components: {Chatsection, Chatheader}
+    }
+</script>
+
+<style scoped>
+.chat{
+  width:100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  background: #f2f2f2;
+}
+</style>
